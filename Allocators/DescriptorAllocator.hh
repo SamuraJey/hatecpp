306caef1d5e9fd4cbd82c45198735625f0884d3f
Jmasko <ch20dima@gmail.com>
2024-04-08 02:58:23 +0500

    best descriptor allocator has no descriptors...

--- Allocators/DescriptorAllocator.hh
+++ Allocators/DescriptorAllocator.hh
@@ -14,16 +14,16 @@ class DescriptorAllocator : public Allocator {
 
    private:
     struct BlockHeader;
-    struct Descriptor;
-    // Менять ссылку на буфер мы не захотим.
-    // С пометкой константы писать логику безопастнее.
+    // Alias for Allocated header size
+    static const size_t alloc_header;
+    static const size_t free_header;
     char* const buffer;
     char* const endBuffer;
     BlockHeader* root;
-    // mark block used and remove from free list
-    void mark_used(BlockHeader* cur);
-    // mark block free and add it to free list before root
-    void mark_free(BlockHeader* newBlock);
+    // add block to free list next to root
+    void add_free(BlockHeader* newBlock);
+    // pop from free list
+    void pop_free(BlockHeader* cur);
     // spaw block from free list with another block
     void list_swap(BlockHeader* from, BlockHeader* to);
 };
